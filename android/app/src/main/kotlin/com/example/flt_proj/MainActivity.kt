62587c15f0d62959e1cbfdcca5cc5c3ca4196ea8
jonghwa <jonghwa0212@gmail.com>
2025-04-03 23:00:01 +0900

    TEST

---
+++ android/app/src/main/kotlin/com/example/flt_proj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flt_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text('Code Factory',
+          style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    ),
+  );
+}
