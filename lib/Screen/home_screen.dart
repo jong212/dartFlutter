1abd6b06f99443ffd3bcdf16da95ebba7928e9ff
jonghwa <jonghwa0212@gmail.com>
2025-04-07 17:01:16 +0900

    Reset Projeect

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flt_proj/const/colors.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,43 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:
-                colors.map((e) =>
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: e,
-                    )).toList()
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  color: Colors.orange,
-                )
-              ],
-            ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children:
-                colors.map((e) =>
-                    Container(
-                      height: 50,
-                      width: 50,
-                      color: e,
-                    )).toList()
-            ),
-          ],
-        )
-      ),
+      
     );
   }
 }
