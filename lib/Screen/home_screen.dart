152fb1aa6471a28ea7b8b311bdfa1276e6e1c472
jonghwa <jonghwa0212@gmail.com>
2025-04-09 23:12:57 +0900

    Top,Bottom 코드 분리 (코드 리펙토링)

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -17,67 +17,85 @@ class HomeScreen extends StatelessWidget {
           child: Column(
 
             children: [
-              Expanded(
-                child: Container(
-                  child: Column(
-                    children: [
-                      Text(
-                        'U&I',
-                        style: TextStyle(
-                          color:Colors.white,
-                          fontFamily: 'parisienne',
-                          fontSize: 80
-                        ),
-                      ),
-                      Text(
-                        '우리 처음 만난날',
-                        style: TextStyle(
-                          color:Colors.white,
-                          fontSize: 30,
-                          fontFamily: 'sunflower'
-                        ),
-                      ),
-                      Text(
-                        '2023.11.23',
-                        style: TextStyle(
-                          color:Colors.white,
-                          fontFamily: 'sunflower',
-                          fontSize: 20
-                        ),
-                      ),
-                      IconButton(
-                        iconSize: 60,
-                        color: Colors.red,
-                        onPressed: (){},
-                        icon:
-                        Icon(
-                            Icons.favorite
-                        )
-                      ),
-                      Text(
-                        'D+1',
-                        style: TextStyle(
-                          color:Colors.white,
-                          fontFamily: 'sunflower',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 50
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Image.asset(
-                    'asset/img/middle_image.png',
-                  ),
-                ),
-              ),
+              _Top(),
+              _Bottom(),
             ],
           ),
         ),
       )
+    );
+  }  
+}
+class _Top extends StatelessWidget {
+  const _Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Expanded(
+        child: Container(
+          child: Column(
+            children: [
+              Text(
+                'U&I',
+                style: TextStyle(
+                    color:Colors.white,
+                    fontFamily: 'parisienne',
+                    fontSize: 80
+                ),
+              ),
+              Text(
+                '우리 처음 만난날',
+                style: TextStyle(
+                    color:Colors.white,
+                    fontSize: 30,
+                    fontFamily: 'sunflower'
+                ),
+              ),
+              Text(
+                '2023.11.23',
+                style: TextStyle(
+                    color:Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 20
+                ),
+              ),
+              IconButton(
+                  iconSize: 60,
+                  color: Colors.red,
+                  onPressed: (){},
+                  icon:
+                  Icon(
+                      Icons.favorite
+                  )
+              ),
+              Text(
+                'D+1',
+                style: TextStyle(
+                    color:Colors.white,
+                    fontFamily: 'sunflower',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 50
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+  }
+}
+
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+        ),
+      ),
     );
   }
 }
