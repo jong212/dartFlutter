138632c88ee61e0593e1c5c2fd9fa1f328e47bd9
jonghwa <jonghwa0212@gmail.com>
2025-04-09 23:07:38 +0900

    UI 디자인 배치

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -6,7 +6,78 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top:true,
+        bottom: true,
+        left: false,
+        right: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+
+            children: [
+              Expanded(
+                child: Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        'U&I',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontFamily: 'parisienne',
+                          fontSize: 80
+                        ),
+                      ),
+                      Text(
+                        '우리 처음 만난날',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontSize: 30,
+                          fontFamily: 'sunflower'
+                        ),
+                      ),
+                      Text(
+                        '2023.11.23',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontFamily: 'sunflower',
+                          fontSize: 20
+                        ),
+                      ),
+                      IconButton(
+                        iconSize: 60,
+                        color: Colors.red,
+                        onPressed: (){},
+                        icon:
+                        Icon(
+                            Icons.favorite
+                        )
+                      ),
+                      Text(
+                        'D+1',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontFamily: 'sunflower',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 50
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  child: Image.asset(
+                    'asset/img/middle_image.png',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
